971b6b4d9b4be4792ee28b5479bd18b8cda2311e
AntiSocialAllien <41641321+AntiSocialAllien@users.noreply.github.com>
2019-04-22 13:34:10 +0300

    Bekalu

---
+++ stemgirls/lib/chatroom.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Chat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Center(
+        child: new Icon(Icons.chat, size: 150.0, color: Colors.lightBlue)
+      )
+    );
+  }
+
+}
---
+++ stemgirls/lib/main.dart
@@ -0,0 +1,61 @@
+import "package:flutter/material.dart";
+import './chatroom.dart' as chat;
+import './facts.dart' as facts;
+import './about.dart' as about;
+
+void main() {
+  runApp(new MaterialApp(
+    
+  ));
+}
+
+class MyTabs extends StatefulWidget {
+  @override
+  MyTabsState createState() => new MyTabsState();
+}
+
+class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
+  
+
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+  
+  
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Games"), 
+      backgroundColor: Colors.lightBlue,
+      bottom: new TabBar(
+        controller: controller,
+        tabs: <Tab>[
+         new Tab(icon: new Icon(Icons.chat)),
+         new Tab(icon: new Icon(Icons.verified_user)),
+         new Tab(icon: new Icon(Icons.favorite)),
+        ]
+       )
+      ),
+      body: new TabBarView(
+        controller: controller,
+        children: <Widget>[
+          new chat.Chat(),
+          new facts.Facts(),
+          new about.About()
+        ],
+      )
+    );
+  }
+}
